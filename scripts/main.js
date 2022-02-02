22b62c24904e604c48d39510d0b9042e623cc6ab
sekkarin <sekkri1234@gmail.com>
2022-02-02 11:34:45 +0700

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// scripts
+ScrollReveal({ duration: 600 })
+ScrollReveal().reveal('.nav-bar', { delay: 200 })
+ScrollReveal().reveal('.delay-1', { delay: 600 })
+ScrollReveal().reveal('.delay-2', { delay: 1200 })
